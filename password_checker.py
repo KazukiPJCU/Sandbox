9eb2fa24fbe2b7a099fc73d13af846474366c12c
KazukiPJCU <Kazuki.Pickersgill@my.jcu.edu.au>
2020-08-27 09:48:38 +1000

    fix error check

--- password_checker.py
+++ password_checker.py
@@ -1,6 +1,8 @@
-min_length=6
+min_length = 6
 password = input("Password: ")
 password_length = len(password)
 while password_length < min_length:
+    print("Incorrect password length minimum length is", min_length)
     password = input("Password: ")
-print(password_length*"*")
+    password_length = len(password)
+print(password_length * "*")
